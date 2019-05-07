81337e65234d00201b06442a83ab18ddefb8dedc
Pritam K Dey <pritam251@gmail.com>
2019-05-07 11:10:04 -0700

    Adding comments/javadoc

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Main Activity class to show string in app
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Show activity_main layout which holds the design to show strings
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
